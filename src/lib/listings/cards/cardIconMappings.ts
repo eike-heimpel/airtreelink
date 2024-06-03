6a20a33a9bc9dbb8e7adb0ae48f8a2b4adee8d91
Eike Heimpel <eike@heimpel.family>
2024-06-03 16:39:06 +0200

    update

--- src/lib/listings/cards/cardIconMappings.ts
+++ src/lib/listings/cards/cardIconMappings.ts
@@ -10,8 +10,10 @@ import WifiIcon from 'virtual:icons/mdi/wifi';
 import PoolIcon from 'virtual:icons/mdi/pool';
 import DogServiceIcon from 'virtual:icons/mdi/dog-service';
 import CityIcon from 'virtual:icons/mdi/city';
-import NaturePeopleIcon from 'virtual:icons/mdi/nature-people';
+import PineTree from 'virtual:icons/mdi/pine-tree';
 import Hiking from 'virtual:icons/mdi/hiking';
+import Coffee from 'virtual:icons/mdi/coffee';
+import Museum from 'virtual:icons/mdi/museum';
 
 export const iconMapping = {
     restaurant: {
@@ -20,16 +22,34 @@ export const iconMapping = {
         bgColor: 'bg-yellow-400',
         filterBy: true
     },
-    'home': {
-        name: 'Home',
-        component: HomeOutlineIcon,
-        bgColor: 'bg-blue-100',
-        filterBy: false
+    coffee: {
+        name: 'Coffee',
+        component: Coffee,
+        bgColor: 'bg-green-400',
+        filterBy: true
+    },
+    'park': {
+        name: 'Park',
+        component: PineTree,
+        bgColor: 'bg-green-200',
+        filterBy: true
+    },
+    'hiking': {
+        name: 'Hiking',
+        component: Hiking,
+        bgColor: 'bg-green-200',
+        filterBy: true
     },
     'beach': {
         name: 'Beach',
         component: BeachIcon,
         bgColor: 'bg-blue-200',
+        filterBy: true
+    },
+    "museum": {
+        name: 'Museum',
+        component: Museum,
+        bgColor: 'bg-purple-100',
         filterBy: true
     },
     'bike': {
@@ -44,23 +64,23 @@ export const iconMapping = {
         bgColor: 'bg-pink-100',
         filterBy: true
     },
+    "sightseeing": {
+        name: 'Sightseeing',
+        component: MapMarkerIcon,
+        bgColor: 'bg-yellow-100',
+        filterBy: true
+    },
     'city': {
         name: 'City',
         component: CityIcon,
         bgColor: 'bg-purple-100',
         filterBy: true
     },
-    'nature-people': {
-        name: 'Nature',
-        component: NaturePeopleIcon,
-        bgColor: 'bg-green-200',
-        filterBy: true
-    },
-    'hiking': {
-        name: 'Hiking',
-        component: Hiking,
-        bgColor: 'bg-green-200',
-        filterBy: true
+    'home': {
+        name: 'Home',
+        component: HomeOutlineIcon,
+        bgColor: 'bg-blue-100',
+        filterBy: false
     },
     'car': {
         name: 'Car',
